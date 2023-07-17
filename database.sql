32456dc9a4215339030001b55a4c01c4623e02e2
Muhammad Ilham <muhilhamm.sidiq@gmail.com>
2023-07-18 01:27:27 +0700

    feat/ilham/add_sql_file

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE refresh_tokens (
+    username VARCHAR(50) UNIQUE NOT NULL,
+    token VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+)
